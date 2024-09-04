0177ef6f8700c338d25619cdbc543786ee0521fa
BLZ-dL <1803840060@qq.com>
2024-09-04 23:14:22 +0800

    Add files via upload

---
+++ test2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+char b = 'x';
+int c = 3;
+short d = -32768;
+int main()
+{
+	printf("%c\n", b);
+	printf("\n");
+	int a = 1;
+	printf("%d\n", a);
+	printf("%d\n", c);
+	printf("%d\n", d);
+}
